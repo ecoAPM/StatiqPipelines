8e37e95edd41842ecec60a2639e7eb80cd24a22e
Steve Desmond <Steve@ecoAPM.com>
2023-07-03 11:57:38 -0400

    Allow copying entire directories from node_modules

--- StatiqPipelines.Tests/CopyFromNPMTests.cs
+++ StatiqPipelines.Tests/CopyFromNPMTests.cs
@@ -16,26 +16,27 @@ public class CopyFromNPMTests
 		context.FileSystem.GetInputFile("/node_modules/y/2.js").OpenWrite();
 
 		var pipeline = new CopyFromNPM(new[] { "x/1.js", "y/2.js" }, "assets/js");
-		var input = pipeline.InputModules.First(m => m is ReadFiles);
+		var input = pipeline.InputModules.Where(m => m is ReadFiles);
 
 		//act
-		var output = await input.ExecuteAsync(context);
+		var tasks = input.Select(async i => await i.ExecuteAsync(context));
+		var output = await Task.WhenAll(tasks);
 
 		//assert
-		var files = output.Select(d => d.Source.FileName.ToString()).ToArray();
+		var files = output.SelectMany(d => d).Select(d => d.Source.FileName.ToString()).ToArray();
 		Assert.Equal(2, files.Length);
 		Assert.Contains("1.js", files);
 		Assert.Contains("2.js", files);
 	}
 
 	[Fact]
-	public async Task CopyToFlattensOutputPath()
+	public async Task CopyToFlattensOutputPathForFiles()
 	{
 		//arrange
 		var docs = new List<IDocument>
 			{
-				new TestDocument(new NormalizedPath("x/1.js")),
-				new TestDocument(new NormalizedPath("y/2.js"))
+				new TestDocument(new NormalizedPath("/node_modules/x/1.js")),
+				new TestDocument(new NormalizedPath("/node_modules/y/2.js"))
 			};
 		var context = new TestExecutionContext();
 		context.SetInputs(docs);
@@ -50,5 +51,29 @@ public class CopyFromNPMTests
 		var outputDocs = output.ToArray();
 		Assert.Equal("assets/js/1.js", outputDocs[0].Destination);
 		Assert.Equal("assets/js/2.js", outputDocs[1].Destination);
+	}
+
+	[Fact]
+	public async Task CopyToDoesNotFlattenOutputPathForDirectories()
+	{
+		//arrange
+		var docs = new List<IDocument>
+			{
+				new TestDocument(new NormalizedPath("/node_modules/x/1.js")),
+				new TestDocument(new NormalizedPath("/node_modules/x/y/2.js"))
+			};
+		var context = new TestExecutionContext();
+		context.SetInputs(docs);
+
+		var pipeline = new CopyFromNPM(new[] { "x" }, "assets/js");
+		var copy = pipeline.ProcessModules.First(m => m is SetDestination);
+
+		//act
+		var output = await copy.ExecuteAsync(context);
+
+		//assert
+		var outputDocs = output.ToArray();
+		Assert.Equal("assets/js/x/1.js", outputDocs[0].Destination);
+		Assert.Equal("assets/js/x/y/2.js", outputDocs[1].Destination);
 	}
 }
--- StatiqPipelines/CopyFromNPM.cs
+++ StatiqPipelines/CopyFromNPM.cs
@@ -5,44 +5,50 @@ namespace ecoAPM.StatiqPipelines;
 
 public class CopyFromNPM : Pipeline
 {
+	private const string NodeModulesDirectory = "node_modules/";
+	private Dictionary<string, ReadFiles> _files;
+
 	/// <summary>
 	/// Copies specific files from a `node_modules` directory to the output
 	/// </summary>
 	/// <param name="paths">The file paths (relative to `node_modules`) to copy</param>
 	/// <param name="output">The path (relative to the output root) where the files will be copied</param>
+	/// <param name="flatten">Flatten all files into the <see cref="output">output</see> directory</param>
 	public CopyFromNPM(IEnumerable<string> paths, string output = "lib")
 	{
+		_files = paths.ToDictionary(p => p, p => new ReadFiles(npmPath(p)));
+
 		Isolated = true;
-		InputModules = new ModuleList
-			{
-				new NodeRestore(),
-				new ReadFiles(npmPath(paths))
-			};
-		ProcessModules = new ModuleList
-			{
-				CopyTo(output)
-			};
-		OutputModules = new ModuleList
-			{
-				new WriteFiles()
-			};
+		InputModules = new ModuleList { new NodeRestore() };
+		InputModules.AddRange(_files.Values);
+
+		ProcessModules = new ModuleList { CopyTo(output) };
+
+		OutputModules = new ModuleList { new WriteFiles() };
 	}
 
-	private static IReadOnlyList<string> npmPath(IEnumerable<string> paths)
-		=> paths.Select(p => IExecutionContext.Current.FileSystem
-				.GetRootDirectory("node_modules").GetFile(p)
-				.Path.FullPath.Replace("\\", "/"))
-			.ToArray();
+	private static string npmPath(string path)
+		=> IExecutionContext.Current.FileSystem
+			.GetRootDirectory(NodeModulesDirectory).GetFile(path)
+			.Path.FullPath.Replace("\\", "/");
 
-	private static SetDestination CopyTo(string output)
+	private SetDestination CopyTo(string output)
 		=> new(SetPath(output));
 
-	private static Config<NormalizedPath> SetPath(string output)
+	private Config<NormalizedPath> SetPath(string output)
 		=> Config.FromDocument(d => NewPath(output, d));
 
-	private static NormalizedPath NewPath(string output, IDocument d)
+	private NormalizedPath NewPath(string output, IDocument d)
 		=> new(OutputPath(output, d));
 
-	private static string OutputPath(string output, IDocument d)
-		=> Path.Combine(output, d.Source.FileName.ToString());
+	private string OutputPath(string output, IDocument d)
+		=> Path.Combine(output, RelativeOutputPath(d));
+
+	private string RelativeOutputPath(IDocument d)
+		=> _files.ContainsKey(RelativePath(d))
+			? d.Source.FileName.ToString()
+			: RelativePath(d);
+
+	private static string RelativePath(IDocument d)
+		=> d.Source.RootRelative.ToString().RemoveStart(NodeModulesDirectory);
 }
